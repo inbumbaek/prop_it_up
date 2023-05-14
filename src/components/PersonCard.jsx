c9f454112dcd836a2a40af5396689db692ea3b8b
inbumbaek <inbumbaek@gmail.com>
2023-05-14 02:48:04 -0700

    create a bday button

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
-
+    
     let {firstName, lastName, age, hairColor} = props;
+    const [number, setNumber] = useState(age);
+    const increaseNumber = () => {
+        setNumber(number+1)
+    }
+
     return(
         <div>
-            <h1>{firstName}, {lastName}</h1>
-            <p>Age: {age}</p>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {number}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={increaseNumber}>Birthday Button for {firstName} {lastName}</button>
         </div>
     );
 }
